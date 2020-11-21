2557f9c77f59f66c98324415c7bc427c1fb74ebd
Nicholas Szymonik <nszymoni@masonlive.gmu.edu>
2020-11-20 23:57:30 -0500

    made bullet go

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour
+{
+    public Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        Debug.Log("moving...");
+        transform.Translate(velocity*Time.deltaTime);
+    }
+
+    /*
+     * Shoots a bullet at the desired velocity at the desired position. 
+     */
+    public static void fire(BulletMovement bulletPrefab, Vector2 pos, Vector2 vel)
+    {
+        Instantiate(bulletPrefab, pos, Quaternion.LookRotation(vel)).velocity = vel;
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : CharacterController
 {
 
+    private void Start()
+    {
+        MoveSpeed = 5;
+    }
 
 
 
