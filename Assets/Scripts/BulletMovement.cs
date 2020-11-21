61134b4f5dd296830d3ea88008effd2d46d7090d
Nicholas Szymonik <nszymoni@masonlive.gmu.edu>
2020-11-21 01:16:30 -0500

    created a simple bullet pattern

--- Assets/Scripts/BulletMovement.cs
+++ Assets/Scripts/BulletMovement.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * A basic, no-frills bullet movement simulator. 
+ * Moves at a constant velocity.
+ * If we want more complex movement, we'll have to use class inheritance.
+ */
 public class BulletMovement : MonoBehaviour
 {
     public Vector2 velocity;
@@ -9,21 +14,22 @@ public class BulletMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-     
+        
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        Debug.Log("moving...");
         transform.Translate(velocity*Time.deltaTime);
+        //transform.eulerAngles = new Vector3(0,0, Mathf.radMathf.Atan2(velocity.y, velocity.x));
     }
 
     /*
      * Shoots a bullet at the desired velocity at the desired position. 
      */
-    public static void fire(BulletMovement bulletPrefab, Vector2 pos, Vector2 vel)
+    public static void fire(GameObject bulletPrefab, Vector2 position, Vector2 newVelocity)
     {
-        Instantiate(bulletPrefab, pos, Quaternion.LookRotation(vel)).velocity = vel;
+        GameObject p = Instantiate(bulletPrefab, position, Quaternion.identity);
+        p.GetComponent<BulletMovement>().velocity = newVelocity;
     }
 }
